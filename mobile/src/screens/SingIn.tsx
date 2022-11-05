847aaa73a2302f3e98a7ad64c54ee3b954b8c7c3
Fanuel Couto <fanuel.couto99@gmail.com>
2022-11-04 21:16:13 -0300

    Adicionado projeto mobile

---
+++ mobile/src/screens/SingIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function SingIn(){
+    return (
+        <Center flex={1} bgColor="gray.900">
+        <Text color="#fff" fontSize={24} fontFamily="heading">
+            SingIn
+        </Text>
+      </Center>
+    );
+};
