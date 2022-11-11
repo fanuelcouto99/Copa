33db4d4b131e29e9ac7920468b56b3c807a05f4a
Fanuel Couto <fanuel.couto99@gmail.com>
2022-11-10 21:29:58 -0300

    Iniciando configuração de autenticação com Google

--- mobile/src/contexts/AuthContexts.tsx
+++ mobile/src/contexts/AuthContexts.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
     name: string;
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
     user: UserProps;
+    isUserLoading: boolean;
     singIn: () => Promise<void>;
 }
 
@@ -21,13 +27,31 @@ export const AuthContext = createContext({} as AuthContextDataProps);
 // Criando função para fazer o login com o Google e compartilhar com a aplicação
 export function AuthContextProvider({ children }: AuthProviderProps) {
 
+    const [isUserLoading, setIsUserLoading] = useState(false);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '409198835429-hh60o5hmuod2i1lf8vn0cgv5jqm7hm9e.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    });
+
     async function singIn() {
-        console.log('Vamos logar!')
-    }
+        try {
+            setIsUserLoading(true);
+            await promptAsync();
+        } catch (error) {
+            console.log(error);
+            throw error;
+        } finally {
+            setIsUserLoading(false);
+        }
+    };
 
     return (
         <AuthContext.Provider value={{
-            singIn, user: {
+            singIn, 
+            isUserLoading,
+            user: {
                 name: "Fanuel Couto",
                 avatarUrl: "https://github.com/fanuelcouto99.png"
             }
